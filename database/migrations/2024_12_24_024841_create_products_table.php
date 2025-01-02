f0a06cdda20fb1129dc2e2de7647a34b4b52e8de
adawiah <r416820@gmail.com>
2025-01-02 22:06:04 +0700

    bismillah

--- database/migrations/2024_12_24_024841_create_products_table.php
+++ database/migrations/2024_12_24_024841_create_products_table.php
@@ -1,27 +1,24 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+namespace App\Models;
 
-return new class extends Migration
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    use HasFactory;
 
     /**
-     * Reverse the migrations.
+     * fillable
+     *
+     * @var array
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
-};
+    protected $fillable = [
+        'image',
+        'title',
+        'description',
+        'price',
+        'stock',
+    ];
+}
